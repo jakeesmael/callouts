13f14461ef9c6e9d1bb553a08c15ea0fe8c17582
Jake Esmael <jakeesmael@gmail.com>
2014-10-20 21:21:43 -0500

    initial commit of sign up functionality

--- app/controllers/Callouts.java
+++ app/controllers/Callouts.java
@@ -1,19 +1,67 @@
 package controllers;
 
+import com.avaje.ebean.Ebean;
+import com.avaje.ebean.Query;
+import com.avaje.ebean.RawSql;
+import com.avaje.ebean.RawSqlBuilder;
+import models.User;
+import play.data.DynamicForm;
+import play.data.Form;
 import play.mvc.*;
 
 import java.util.Map;
 import java.util.Set;
+import java.util.List;
 
 /**
  * Created by jakeesmael on 10/19/14.
  */
 public class Callouts extends Controller {
+
+	public static class UserForm {
+		public String username;
+		public String password;
+	}
+
 	public static Result login() {
 		return ok(views.html.login.render());
 	}
 
+	public static Result signUpGet() {
+		return ok(views.html.signup.render(false));
+	}
+
+	public static Result signUpPost() {
+		Form<UserForm> userFormForm = Form.form(UserForm.class);
+		UserForm userForm = userFormForm.bindFromRequest().get();
+
+		boolean validSignUp = true;
+
+		if (userForm.username == null || userForm.password == null) {
+			validSignUp = false;
+		}
+
+		String sql = "select * from users where username = \"" + userForm.username + "\";";
+		RawSql rawSql = RawSqlBuilder.unparsed(sql).create();
+		Query<User> query = Ebean.find(User.class).setRawSql(rawSql);
+
+		//query.findList();
+		if (userList.isEmpty()) {
+			addUser(userForm);
+			return ok(views.html.test.render("created " + userForm.username + " " + userForm.password));
+		}
+		else {
+			validSignUp = false;
+			return ok(views.html.signup.render(validSignUp));
+		}
+
+	}
+
+	public static void addUser(UserForm user) {
+
+	}
+
 	public static Result authenticate() {
-		return ok(views.html.test.render());
+		return ok(views.html.test.render(""));
 	}
 }
