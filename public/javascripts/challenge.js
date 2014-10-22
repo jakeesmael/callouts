56c4b95aaec400645110347f27cd24522e0ff0b0
EngrishMajor <ken.yht@gmail.com>
2014-10-22 00:13:49 -0500

    edited profile.css

--- public/javascripts/challenge.js
+++ public/javascripts/challenge.js
@@ -1,3 +1,23 @@
 /**
  * Created by ken on 10/21/14.
  */
+$(document).ready(function() {
+    console.log("challenge.js included");
+
+    $('#challenge-button').click(function() {
+        console.log("button clicked");
+        $('#challenge-overlay').fadeToggle("fast");
+    });
+
+    $('#challenge-overlay').click(function() {
+        $('#challenge-overlay').fadeToggle("fast");
+    });
+
+    $('#datetimepicker1').datetimepicker();
+});
+
+console.log("not ready");
+
+if (window.console) {
+    console.log("asdofkjawoifh");
+}
