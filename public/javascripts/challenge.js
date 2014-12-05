0247d47a93d06d0a8581abcea12447f989dfa03a
EngrishMajor <ken.yht@gmail.com>
2014-12-04 21:02:30 -0600

    rename challenge.js to profile.js

--- public/javascripts/challenge.js
+++ public/javascripts/challenge.js
@@ -19,4 +19,31 @@ $(document).ready(function() {
         autoclose: true,
         startDate: nowDate
     });
+
+    window.fbAsyncInit = function() {
+        FB.init({
+            appId      : '653906481394603',
+            xfbml      : true,
+            version    : 'v2.2'
+        });
+
+        FB.getLoginStatus(function(response) {
+            // The response object is returned with a status field that lets the
+            // app know the current login status of the person.
+            if (response.status === 'connected') {
+                $("#fb-status").text("Your account is linked.");
+            } else {
+                // The person is not logged into Facebook, so we're not sure if they are logged into this app or not.
+                console.log("You're not connected to Facebook!");
+            }
+        });
+    };
+
+    (function(d, s, id){
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
 });
